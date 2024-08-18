1e7acea9e41c300eeb52bcaa6d014c8490db77c2
HAKTAN <alipaycu03@gmail.com>
2024-08-18 15:51:01 +0300

    Typedef

---
+++ typedef.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+typedef int number_t;
+typedef string text_t;
+using books_t = string;
+
+
+int main (){
+
+    text_t name = "Ali";
+    number_t age = 14;
+    // books_t savasVeBaris = "Dostoyevski";
+
+    cout << name << endl;
+    cout << age << endl;
+    // cout << savasVeBaris << endl;
+}
