648bc059dc04977ae007d1fdbd5aec2c7588ba96
HAKTAN <alipaycu03@gmail.com>
2024-08-27 12:29:45 +0300

    Overload Functions

---
+++ overload_functions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void bakePizza();
+void bakePizza(string topping1);
+void bakePizza(string topping1, string topping2);
+
+int main(){
+    bakePizza("peperonni", "mushroom");
+
+}
+void bakePizza(){
+    cout << "Here is your pizza!";
+}
+void bakePizza(string topping1){
+    cout << "Here is your "<< topping1 << "pizza!" << "\n";
+}
+void bakePizza(string topping1 , string topping2){
+    cout << "Here is your "<< topping1 << " and "<< topping2 <<" pizza!" << "\n\n";
+}
