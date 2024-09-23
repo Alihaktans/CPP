ae798bb1e3863b6d4f37629cce60ad92555ef105
HAKTAN <alipaycu03@gmail.com>
2024-09-23 16:28:44 +0300

    Ekleme

---
+++ q1.cpp
@@ -0,0 +1,28 @@
+// #include <stdlib.h>
+// #include <stdio.h>
+// enum {sunday, monday};
+// int main()
+// {
+// int i = 1;
+// do
+// {
+// 	printf("%d\n", i);
+// 	i++;
+// 	if (i < 15)
+// 		continue;
+// } while (false);
+
+// getchar();
+// return 0;
+// }
+
+// #include <stdio.h>
+// enum day {sunday = 1, tuesday, wednesday, thursday, friday, saturday};
+
+// int main()
+// {
+// 	enum day d = thursday;
+// 	printf("The day number stored in d is %d", d);
+// 	return 0;
+// }
+
